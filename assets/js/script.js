97917c08e786a8f0e9128b95554dd6d72cae001f
Misha <73886737+misha244@users.noreply.github.com>
2021-03-31 18:16:51 +0100

    set a todo list

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+//TODO
+//declare timeblock element
+
+//set up a function that adjusts time block classes accordingly to time //use for each
+//const updateTimeBlocks = () => {}
+
+//saveBtn - save to local storage and load on refresh
