94b359008a69975110658550dad8f6f425e7d5bc
Misha <73886737+misha244@users.noreply.github.com>
2021-04-01 16:08:00 +0100

    use an if/else to set classes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,9 @@
 const timeBlockElement = document.getElementById("timeBlockDiv");
 const saveButton = document.getElementById("saveBtn");
 const currentTime = document.getElementById("currentDay");
+const containerParent = document.getElementById("container");
+// target data attribute
+const textAreas = containerParent.children(".text-area");
 
 // set current time fn with moment.js
 const setCurrentTime = () => {
@@ -16,8 +19,14 @@ const updateTimeBlocks = () => {
   const currentHour = moment().format("H");
   // convert str number to int number
   const currentHourInt = parseInt(currentHour);
-  // add time classes to text
-  const setTimeClass = () => {};
+  // set time classes to text
+  const setTimeClass = (index) => {
+    const myTimeString = textAreas[index].dataset.mytime;
+    // convert to int
+    const myTimeInt = parseInt(myTimeString);
+    if (myTimeInt === currentHourInt) {
+    }
+  };
 };
 
 // create fn to save input to local storage
