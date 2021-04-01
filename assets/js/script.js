889c6632cf47cf3b90004d9260e285a00d4d910e
Misha <73886737+misha244@users.noreply.github.com>
2021-04-01 10:39:01 +0100

    added to todo list

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,10 @@
 //TODO
 //declare timeblock element
 
-//set up a function that adjusts time block classes accordingly to time //use for each
+//set up a function that adjusts time block classes accordingly to time //use a for each
+///set class names
 //const updateTimeBlocks = () => {}
 
 //saveBtn - save to local storage and load on refresh
+/// event listener
+//set text content of textarea
