7327985dd78d2dadea6d578d67a4cf8aaf04068e
Misha <73886737+misha244@users.noreply.github.com>
2021-04-01 16:15:25 +0100

    update todo

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,8 +10,6 @@ const setCurrentTime = () => {
   currentTime.textContent = moment().format("dddd, MMMM Do YYYY, LTS");
   return currentTime;
 };
-// time blocks array
-// timeBlocks = [];
 
 // update time blocks fn - adjust colours according to the current hour
 const updateTimeBlocks = () => {
@@ -47,10 +45,6 @@ const setSaveButton = () => {
 
 window.addEventListener("load", setCurrentTime);
 //TODO
-//declare timeblock element
-//get current time with moment.js
-
-///set class names
 
 //saveBtn - save to local storage and load on refresh
 /// event listener
