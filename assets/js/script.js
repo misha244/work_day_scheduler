c128469db5cc0aa6e61825abca9985f064d9837d
Misha <73886737+misha244@users.noreply.github.com>
2021-04-02 18:28:09 +0100

    set elements for each time block

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,15 @@
 const containerDiv = $(".container");
 const textAreas = containerDiv.children().children("textarea");
-
+// set elements for each time block
+const nineAmElement = document.querySelector("#user-input-9");
+const tenAmElement = document.querySelector("#user-input-10");
+const elevenAmElement = document.querySelector("#user-input-11");
+const twelevePmElement = document.querySelector("#user-input-12");
+const onePmElement = document.querySelector("#user-input-13");
+const twoPmElement = document.querySelector("#user-input-14");
+const threePmElement = document.querySelector("#user-input-15");
+const fourPmElement = document.querySelector("#user-input-16");
+const fivePmElement = document.querySelector("#user-input-17");
 // set current time fn with moment.js
 const setCurrentTime = () => {
   const currentTime = moment().format("dddd, MMMM Do YYYY");
@@ -30,10 +39,14 @@ const updateTimeBlocks = () => {
   textAreas.each(setTimeClass);
 };
 
-// save input into local storage using on click event
-$(".saveBtn").click(function () {
-  const textInputItem = localStorage.getItem();
-});
+//
+//const saveTasksToLocal = (event) => {
+//  event.preventDefault();
+//
+//  const target = $(event.target);
+//};
+
 // call functions with jquery
 $(document).ready(setCurrentTime);
 $(document).ready(updateTimeBlocks);
+$(".saveBtn").on("click", saveTasksToLocal);
